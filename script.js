6347a8b344c39e85e6702cf33a4de459ddbabe96
Jayeshrakhonde07 <jayeshrakhonde05@gmail.com>
2026-01-30 15:46:10 +0530

    all code

--- script.js
+++ script.js
@@ -1,21 +1,70 @@
 let boxes = document.querySelectorAll(".box");
-let resetBtn = document.querySelector("#reset-btn");
+let resetbtn = document.getElementById("reset-btn");
+let msg = document.querySelector(".msg-container");
+let winnerMsg = document.getElementById("winner-text");
+let newBtn = document.getElementById("new-btn");
 let turn0 = true;
 
+const winPatterns = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
+const enableBoxes = () => {
+  boxes.forEach((box) => {
+    box.disabled = false;
+    box.innerText = "";
+  });
+  msg.classList.add("hide");
+  turn0 = true;
+};
 
+const disableBoxes = () => {
+  boxes.forEach((box) => {
+    box.disabled = true;
+  });
+};
 
-Array.from(boxes).forEach((box) => {
-    box.addEventListener("click",() => {
-        if(turn0){
-            box.innerText = "0";
-            turn0 = false;
-        }
-        else {
-            box.innerText = "X";
-            turn0 = true;
-        }
-        box.disabled  = true;
-        checkWinner = 0;
-    })
-})
+const showWinner = (winner) => {
+  msg.classList.remove("hide");
+  winnerMsg.innerText = `Congratulations Winner is player ${winner} 🎉🎉`;
+  disableBoxes();
+};
+
+const checkWinner = () => {
+  for (let pattern of winPatterns) {
+    let pos1 = boxes[pattern[0]].innerText;
+    let pos2 = boxes[pattern[1]].innerText;
+    let pos3 = boxes[pattern[2]].innerText;
+
+    if (pos1 != "" && pos2 != "" && pos3 != "") {
+      if (pos1 === pos2 && pos2 === pos3) {
+        showWinner(pos1);
+        return;
+      }
+    }
+  }
+};
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (turn0) {
+      box.innerText = "O";
+      turn0 = false;
+    } else {
+      box.innerText = "X";
+      turn0 = true;
+    }
+    box.disabled = true;
+    checkWinner();
+  });
+});
+
+resetbtn.addEventListener("click", enableBoxes);
+newBtn.addEventListener("click", enableBoxes);
